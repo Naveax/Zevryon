11dcfa1e8ab72ab5c2adc198fcecad14da1ae500
Naveax <79841922+Naveax@users.noreply.github.com>
2026-07-25 01:16:18 +0300

    Z2C-1: expose level-split statistics

--- src/shaping_run_plan.hpp
+++ src/shaping_run_plan.hpp
@@ -59,6 +59,7 @@ struct ShapingRunPlanStats {
     std::uint64_t script_splits{0};
     std::uint64_t font_splits{0};
     std::uint64_t direction_splits{0};
+    std::uint64_t level_splits{0};
 };
 
 class ShapingRunPlan final {
